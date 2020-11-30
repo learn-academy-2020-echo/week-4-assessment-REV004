81e17c3bee26e31e5bc8c39665c260f1e47185ac
REV004 <rsier004@gmail.com>
2020-11-30 08:25:58 -0800

    finish java

--- code_challenges.rb
+++ code_challenges.rb
@@ -3,53 +3,105 @@
 
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest. Use the test variables provided.
 
-full_arr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
-# Expected output: [-9, 7, 9, 199]
-full_arr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
-# Expected output: [-823, 7, 23]
+# full_arr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
+# # Expected output: [-9, 7, 9, 199]
+
+# def new_array array
+#     array.select do | value |
+#         if value.odd?
+#         end
+#     end
+
+# p new_array full_arr1
+
+#     full_arr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
+# # # Expected output: [-823, 7, 23]
+
+
+# def new_array array
+#     array.select do | value |
+#         value.odd?
+#         end
+#     end
+
+# p new_array full_arr2
+
+#I know I have it im just missing something as it doesnt take on the strings or letters///
+
+# # --------------------2) Create a method that takes in an array of words and a letter and returns all the words that contain that particular letter.
+
+# beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# # letter_o = 'o'
+# # # Expected output: ['coffee', 'soda water']
+# # letter_a = 'a'
+# # # Expected output: ['tea', 'soda water']
+
+# # def finder array
+# #     array.select { |word| word.include? "o" }
+# #   end
+
+# #   p finder beverages_array
+
+# def finder array 
+#     array.select {|word| word.include? "a"}
+# end
+
+# p finder beverages_array
+
+
+# # -------------------3) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+# album1 = 'Rubber Soul'
+# # # Expected output: 'Rbbr Sl'
+# album2 = 'Sgt Pepper'
+# # # Expected output: 'Sgt Pppr'
+# album3 = 'Abbey Road'
+# # # Expected output: 'bby Rd'
+
+# def remove_vowel string
+#     string.delete "aeiouAEIOU"
+#   end
+
+#   p remove_vowel album1
+#   p remove_vowel album2
+#   p remove_vowel album3
 
 
 
 
+# # -------------------4) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
 
-# --------------------2) Create a method that takes in an array of words and a letter and returns all the words that contain that particular letter.
-
-beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
-letter_o = 'o'
-# Expected output: ['coffee', 'soda water']
-letter_a = 'a'
-# Expected output: ['tea', 'soda water']
+# nums_array1 = [42, 7, 27]
+# # Expected output: 76
 
 
+# def new_array array
+#     sum = 0
+#     array.each do |number|
+#       sum = sum + number
+#     end
+#     sum
+# end 
+
+# p new_array nums_array1
+
+# nums_array1 = [25, 17, 47, 11]
+# # Expected output: 100
 
 
+# def new_array array
+#     sum = 0
+#     array.each do |number|
+#       sum = sum + number
+#     end
+#     sum
+# end 
 
-# -------------------3) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
-
-album1 = 'Rubber Soul'
-# Expected output: 'Rbbr Sl'
-album2 = 'Sgt Pepper'
-# Expected output: 'Sgt Pppr'
-album3 = 'Abbey Road'
-# Expected output: 'bby Rd'
-
-
-
-
-
-# -------------------4) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
-
-nums_array1 = [42, 7, 27]
-# Expected output: 76
-
-nums_array1 = [25, 17, 47, 11]
-# Expected output: 100
+# p new_array nums_array1
 
 
 
-
-
-# -------------------5) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forwards or backwards. Use the test variables provided.
+# # -------------------5) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forwards or backwards. Use the test variables provided.
 
 is_palindrome1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -58,6 +110,18 @@ is_palindrome2 = 'LEARN'
 is_palindrome3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
 
-# STRETCH
-is_palindrome4 = 'Was it a cat I saw'
-# Expected output: Was it a cat I saw is a palindrome'
+def palindrome_test string
+    if string == string.reverse
+      put ' #{string} is a palindrome'
+    else
+      puts  '#{string} is not a palindrome' 
+    end
+   end
+
+p palindrome_test is_palindrome1
+
+#it doesnt fire correctly I dont wanna look it up 
+
+# # STRETCH
+# is_palindrome4 = 'Was it a cat I saw'
+# # Expected output: Was it a cat I saw is a palindrome'
--- interview-questions.md
+++ interview-questions.md
@@ -1,59 +1,54 @@
 # ASSESSMENT 4: Interview Practice Questions
+
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory.
 
-Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.  
+Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
 1. Are Ruby methods accessors or mutators?
 
-  Your answer:
+Your answer: theyre mutators
 
-  Researched answer:
-
-
+Researched answer:
 
 2. What is a block in Ruby?
 
-  Your answer:
+Your answer: is a group of statements that are called with a defined function method
 
-  Researched answer:
-
-
+Researched answer:A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call; the block is written starting on the same line as the method call's last parameter.
 
 3. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+Your answer: that its not called since ruby is a last order function
 
-  Researched answer:
-
-
+Researched answer: when return isn’t explicitly called within a method then Ruby returns the value of the last executed instruction in the method
 
 4. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+Your answer:object is defined to a specific set of data given to a variable and functional programming is the same and doesnt change.
 
-  Researched answer:
-
-
+Researched answer:
+functional programming :
+Functional programming is the form of programming that attempts to avoid changing state and mutable data. In a functional program, the output of a function should always be the same, given the same exact inputs to the function. This is because the outputs of a function in functional programming purely relies on arguments of the function, and there is no magic that is happening behind the scenes. This is called eliminating side effects in your code.
+OOP:Object oriented programming is a programming paradigm in which you program using objects to represent things you are programming about (sometimes real world things). These objects could be data structures. The objects hold data about them in attributes. The attributes in the objects are manipulated through methods or functions that are given to the object.
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+Your answer: a class is a set of information that describe an object and when called it pulls out that information that is stored and an object is defined
 
-  Researched answer:
-
-
+Researched answer:
+A class is the blueprint from which individual objects are created. In object-oriented terms, we say that your vehicle is an instance of the class of objects known as vehicle. A vehicle can also have certain functions, such as halting, driving, and speeding. Even these functions form the data members of the class Vehicle. You can, therefore, define a class as a combination of characteristics and functions.
+an object is a set of information given to one defined object.
 
 6. STRETCH: What is Duck Typing?
 
-  Your answer:
+Your answer: where a duck runs your program
 
-  Researched answer:
-
-
+Researched answer: Duck typing means that an operation does not formally specify the requirements that its operands have to meet, but just tries it out with what is given.
 
 ## Looking Ahead: Terms for Next Week
+
 - PostgreSQL
 - API
 - CRUD
